122294a05fac272c4f58f680f4f863f6fc507308
jamierose <jrose111284@gmail.com>
2016-05-07 10:43:08 +0100

    working app that can guess how many fingers the computer is holding up

--- guess my number/ViewController.swift
+++ guess my number/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var number: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func guess(sender: AnyObject) {
+      let randomNumber = arc4random_uniform(10)
+       let guessInt = Int(number.text!)
+        if guessInt != nil {
+            if Int(randomNumber) == guessInt{
+                answer.text = "congrats you guessed right"
+            } else {
+                answer.text = "nope it was \(randomNumber)"
+            }
+        } else {
+            answer.text = "please enter a number 1-9 please"
+        }
+        
+    
+    }
 
+    @IBOutlet weak var answer: UILabel!
 }
 
